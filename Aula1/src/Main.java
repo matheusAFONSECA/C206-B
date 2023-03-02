65cdbef72c86fcc2640f7188336d82045ac9470f
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-02 00:29:53 -0300

    Aula introdutória de POO

---
+++ Aula1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ola Java!");
+
+        int x = 10;
+        System.out.println(x);
+
+        int y = 10;
+        int z = x + y;
+
+        System.out.println(z);
+
+    }
+
+}
