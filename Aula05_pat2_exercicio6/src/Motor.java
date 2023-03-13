472f3ef31f8b58cc201c048fb95599fa67d7f42c
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-12 23:07:21 -0300

    Classe motor pronta

--- Aula05_pat2_exercicio6/src/Motor.java
+++ Aula05_pat2_exercicio6/src/Motor.java
@@ -1,5 +1,14 @@
 public class Motor {
     // declaração de var
-    String cilindradas;  // cilindradas do motor
+    String cilindradas;      // cilindradas do motor
+    float velocidadeMaxima;  // velocidade máxima que o motor consegue entregar
+
+    // metodos
+    void mostraInfo(){  // mostra as informações do motor do Kart
+
+        System.out.println("Cilindradas do Motor: " + cilindradas);
+        System.out.println("Velocidade máxima do Motor: " + velocidadeMaxima);
+
+    }
 
 }
