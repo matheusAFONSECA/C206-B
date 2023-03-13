06207d9e532b5c4379391cb794c8161069890411
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-12 23:13:59 -0300

    Classe Piloto pronta

--- Aula05_pat2_exercicio6/src/Piloto.java
+++ Aula05_pat2_exercicio6/src/Piloto.java
@@ -1,2 +1,13 @@
 public class Piloto {
+
+    // declaração de var
+    String nome;    // nome do piloto
+    boolean vilão;  // diz se o piloto será um vilão ou não
+
+    // métodos da classe Piloto
+    void soltaSuperPoder(){     // faz o piloto soltar o super poder
+
+        System.out.println("SUPER PODER LANÇADO!!!!");
+
+    }
 }
