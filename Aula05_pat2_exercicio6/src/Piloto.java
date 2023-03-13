8d3fa486eeb8dfe8ee139e8e24a057270265abeb
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-13 10:56:37 -0300

    Tinha nomeado errado a variável boolean vilao

--- Aula05_pat2_exercicio6/src/Piloto.java
+++ Aula05_pat2_exercicio6/src/Piloto.java
@@ -2,7 +2,7 @@ public class Piloto {
 
     // declaração de var
     String nome;    // nome do piloto
-    boolean vilão;  // diz se o piloto será um vilão ou não
+    boolean vilao;  // diz se o piloto será um vilão ou não
 
     // métodos da classe Piloto
     void soltaSuperPoder(){     // faz o piloto soltar o super poder
