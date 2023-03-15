605518fcd162190a5f06608064c88cfc8cabd076
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-15 17:59:39 -0300

    Já podemos adiciona informações sobre as cilindradas e velocidade máxima do motor

--- Aula05_pat2_exercicio6/src/Main.java
+++ Aula05_pat2_exercicio6/src/Main.java
@@ -17,10 +17,12 @@ public class Main {
         p1.vilao = true;
         System.out.println("Qual o nome do vilão? ");
         p1.nome = entrada.nextLine();
+        kart1.nome = "Kartvil";
 
         p2.vilao = false;
         System.out.println("Qual o nome do herói? ");
         p2.nome = entrada.nextLine();
+        kart1.nome = "Karteroi";
 
         // definindo qual kart cada um irá dirigir
         kart1.piloto = p1;
@@ -38,6 +40,31 @@ public class Main {
             }
         }
 
+        System.out.println("Quantas cilindradas tem o motor do " + p2.nome + ":");
+        kart2.motor.cilindradas = entrada.nextLine();
+        if(!Objects.equals(kart2.motor.cilindradas, "50")){
+            if(!Objects.equals(kart2.motor.cilindradas, "100")){
+                if(!Objects.equals(kart2.motor.cilindradas, "150")){
+                    System.out.println("Valor de cilindradas inválido, entre novamente com o valor: ");
+                    kart1.motor.cilindradas = entrada.nextLine();
+                }
+            }
+        }
+
+        // definindo a velocidade dos motores
+        System.out.println("Qual a velocidade que tem o motor do " + p1.nome + ":");
+        kart1.motor.velocidadeMaxima = entrada.nextInt();
+        if(kart1.motor.velocidadeMaxima < 0 || kart1.motor.velocidadeMaxima > 150){
+            System.out.println("Velocida inválida, declare novamemte o valor:");
+            kart1.motor.velocidadeMaxima = entrada.nextInt();
+        }
+
+        System.out.println("Qual a velocidade que tem o motor do " + p2.nome + ":");
+        kart2.motor.velocidadeMaxima = entrada.nextInt();
+        if(kart2.motor.velocidadeMaxima < 0 || kart2.motor.velocidadeMaxima > 150){
+            System.out.println("Velocida inválida, declare novamemte o valor:");
+            kart1.motor.velocidadeMaxima = entrada.nextInt();
+        }
 
     }
 }
