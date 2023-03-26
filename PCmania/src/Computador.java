47f0ad28a7875608017c6a4a4c30e1ff84538e72
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-26 14:37:40 -0300

    Falta  colocar os atributos e colocar os metodos

--- PCmania/src/Computador.java
+++ PCmania/src/Computador.java
@@ -2,5 +2,5 @@ public class Computador {
     // Outra classe principal para esse exercício
     // Computador é a classe que guarda as informações que são pertinentes ao Cliente
     // Responsável por guardar as informações dos PCs
-    
+
 }
