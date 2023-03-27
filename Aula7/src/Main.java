fc0800df06add56c586197d163b86358c7e3c403
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-03-27 00:30:14 -0300

    Coloquei a descrição do que o código fará

--- Aula7/src/Main.java
+++ Aula7/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        // código que vai fazer instaciar uma banda e adicionar a ela seus instegrantes, empresario e
+        // estilo musical
     }
 }
