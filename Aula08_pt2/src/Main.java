5cca40c8cd8da24c31c6fbb83c1b384f44d7d766
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-02 23:25:15 -0300

    Pacotes e classes importadas

--- Aula08_pt2/src/Main.java
+++ Aula08_pt2/src/Main.java
@@ -1,5 +1,12 @@
+// importando os pacotes e as classes contidas para a instanciação
+import br.inatel.cdg.inimigo.Asteroide;
+import br.inatel.cdg.jogador.Nave;
+
 public class Main {
     // código que simula a parte de lógica um jogo onde uma nave destroi os asteiroides
+    // nesse código devemos usar os conceitos aprendidos de pacotes e controle para fazer
+    // o exercício proposto
+
     public static void main(String[] args) {
 
     }
