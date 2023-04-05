2e6a71b2c75246f0d201d87b556b615522863f9a
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-05 19:25:11 -0300

    Segundo exercício proposto na aula que utiliza de pacotes

---
+++ Aula08_pt3_exercicio/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // código proposto em um exercicio para recriarmos a lógica do jogo
+        // Arkanoid usando pacotes, construtores e modificadores de acesso
+    }
+}
