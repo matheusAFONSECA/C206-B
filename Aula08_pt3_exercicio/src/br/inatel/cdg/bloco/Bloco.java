b4595ae9e1207fcc21ed1bdf188bd66ecae992ff
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-06 23:04:46 -0300

    Pacotes e classes principais criadas

---
+++ Aula08_pt3_exercicio/src/br/inatel/cdg/bloco/Bloco.java
@@ -0,0 +1,5 @@
+package br.inatel.cdg.bloco;
+
+public class Bloco {
+    // Código que simula a lógica de um bloco no jogo Arkanoid
+}
---
+++ Aula08_pt3_exercicio/src/br/inatel/cdg/jogador/Jogador.java
@@ -0,0 +1,5 @@
+package br.inatel.cdg.jogador;
+
+public class Jogador {
+    // Código que simula a lógica de um bloco no jogo Arkanoid
+}
