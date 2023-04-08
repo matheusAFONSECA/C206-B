fb90cab76364f957c3251b5406ccb6eea2af6a25
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-07 23:40:13 -0300

    Pacotes importados

--- Aula08_pt3_exercicio/src/Main.java
+++ Aula08_pt3_exercicio/src/Main.java
@@ -1,3 +1,8 @@
+// Importando as classes dos pacotes
+import br.inatel.cdg.jogador.Jogador;
+import br.inatel.cdg.bloco.Bloco;
+import br.inatel.cdg.Sistema.Conversor;
+
 public class Main {
     public static void main(String[] args) {
         // código proposto em um exercicio para recriarmos a lógica do jogo
