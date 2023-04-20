3ad9d3d91f257086b2f9837758a9760abb448c9a
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-19 22:05:58 -0300

    Exercicio pronto

--- Aula10/src/Main.java
+++ Aula10/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Cachorro cachorro = new Cachorro("ZEUS", 7);
+        cachorro.mostraInfo();
+        cachorro.emitirSom();
+
+        Lontra lontra = new Lontra("JUJU", 15);
+        lontra.mostraInfo();
+        lontra.emitirSom();
+
+        Boi boi = new Boi("ZE", 20);
+        boi.mostraInfo();
+        boi.emitirSom();
+
     }
 }
