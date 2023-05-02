2ab868f87236139ae9cba0ba301f2c17f59c739c
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-05-02 14:38:35 -0300

    Exercício sobre Collections e ArrayLists

--- Aula12/src/Main.java
+++ Aula12/src/Main.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        List<Double>
+        // criando a ArrayList
+        List<Double> listaDouble = new ArrayList<>();
+
+
+        // adicionando 5 numeros aleatórios na lista
+        for(int i = 0; i < 5; i++){
+            listaDouble.add(new Random().nextDouble()*100);
+        }
+
+        // mostrando os elementos que foram adicionados no ArrayList
+        System.out.println("Antes da ordenação: ");
+        for(Double double1 : listaDouble){
+            System.out.println("\t" + double1);
+        }
+
+        // ordenando o ArrayList de forma decrescente
+        Collections.sort(listaDouble, Collections.reverseOrder());
+        System.out.println("Depois da ordenação decrescente");
+        System.out.println("Antes da ordenação: ");
+        for(Double double1 : listaDouble){
+            System.out.println("\t" + double1);
+        }
 
     }
 }
