d02de027cd440c01499c74ac2c24dc49bbc70fca
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-05-05 00:02:53 -0300

    Exercício mostrando a eficiência de usar ArrayList, HashSet e HashMap

--- Aula13/src/Main.java
+++ Aula13/src/Main.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         // var aux
-        int QTD = 100000;
+        int QTD = 400000;
 
         // criando uma ArrayList
         List<Integer> array = new ArrayList<>();
@@ -27,35 +26,83 @@ public class Main {
         // pegando o tempo inicial para adicionar elementos na ArrayList
         TI = System.currentTimeMillis();
 
-        // adicionando valores na ArrayList
+        // buscando valores na ArrayList
         for (int i = 0; i < QTD; i++){
-            array.get(i);
+            array.contains(i);
         }
 
-        // pegando o tempo final para adicionar elementos na ArrayList
+        // pegando o tempo final para buscar elementos na ArrayList
         TF = System.currentTimeMillis();
 
-        // saindo o tempo para adicionar elmentos na ArrayList
+        // saindo o tempo para buscar elmentos na ArrayList
         TT = TF - TI;
         System.out.println("TEMPO PARA BUSCAR ELEMENTOS NA ARRAYLIST: " + TT + "ms");
 
-        // criando uma ArrayList
-        List<Integer> array = new ArrayList<>();
+        // criando um HashSet
+        Set<Integer> hashSet = new HashSet<Integer>();
 
-        // pegando o tempo inicial para adicionar elementos na ArrayList
-        long TI = System.currentTimeMillis();
+        // pegando o tempo inicial para adicionar elementos no HashSet
+        TI = System.currentTimeMillis();
 
-        // adicionando valores na ArrayList
+        // adicionando valores no HashSet
         for (int i = 0; i < QTD; i++){
-            array.add(i);
+            hashSet.add(i);
         }
 
-        // pegando o tempo final para adicionar elementos na ArrayList
-        long TF = System.currentTimeMillis();
+        // pegando o tempo final para adicionar elementos no HashSet
+        TF = System.currentTimeMillis();
 
-        // saindo o tempo para adicionar elmentos na ArrayList
-        long TT = TF - TI;
-        System.out.println("TEMPO PARA ADICIONAR ELEMENTOS NA ARRAYLIST: " + TT + "ms");
+        // saindo o tempo para adicionar elmentos na HashSet
+        TT = TF - TI;
+        System.out.println("TEMPO PARA ADICIONAR ELEMENTOS NO HASHSET: " + TT + "ms");
+
+        // pegando o tempo inicial para buscar elementos no HashSet
+        TI = System.currentTimeMillis();
+
+        // buscando valores no HashSet
+        for (int i = 0; i < QTD; i++){
+            hashSet.contains(i);
+        }
+
+        // pegando o tempo final para buscar elementos no HashSet
+        TF = System.currentTimeMillis();
+
+        // saindo o tempo para buscar elmentos na HashSet
+        TT = TF - TI;
+        System.out.println("TEMPO PARA BUSCAR ELEMENTOS NO HASHSET: " + TT + "ms");
+
+        // criando um HashMap
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        // pegando o tempo inicial para adicionar elementos no HashMap
+        TI = System.currentTimeMillis();
+
+        // adicionando valores no HashMap
+        for (int i = 0; i < QTD; i++){
+            hashMap.put(i, i);
+        }
+
+        // pegando o tempo final para adicionar elementos no HashMap
+        TF = System.currentTimeMillis();
+
+        // saindo o tempo para adicionar elmentos na HashMap
+        TT = TF - TI;
+        System.out.println("TEMPO PARA ADICIONAR ELEMENTOS NO HASHMAP: " + TT + "ms");
+
+        // pegando o tempo inicial para buscar elementos no HashMap
+        TI = System.currentTimeMillis();
+
+        // buscando valores no HashMap
+        for (int i = 0; i < QTD; i++){
+            hashMap.containsKey(i);
+        }
+
+        // pegando o tempo final para buscar elementos no HashMap
+        TF = System.currentTimeMillis();
+
+        // saindo o tempo para buscar elmentos na HashMap
+        TT = TF - TI;
+        System.out.println("TEMPO PARA BUSCAR ELEMENTOS NO HASHMAP: " + TT + "ms");
 
     }
 }
