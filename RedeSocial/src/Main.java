7b9c6e52979b5edd27b8137cc9028898bb84aa6f
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-05-28 20:10:58 -0300

    Exercicio OK

--- RedeSocial/src/Main.java
+++ RedeSocial/src/Main.java
@@ -20,98 +20,153 @@ public class Main {
         usuario.Usuario(redeSociais);
 
         // fazendo o tratamento de erros e usando uma função de cada rede social
-        try{
-            System.out.println("Olaaa " + usuario.getNome() + ", seja bem vindo!");
-            System.out.println("Qual ação você gostaria de realizar nas suas redes sociais?");
+        boolean flag2 = true;       // controle do try -> catch
+        while(flag2){
+            try{
+                flag2 = false;
+                System.out.println("Olaaa " + usuario.getNome() + ", seja bem vindo!");
+                System.out.println("Qual ação você gostaria de realizar nas suas redes sociais?");
 
-            Scanner in = new Scanner(System.in);        // Scanner para entrada de dados
-            boolean flag = true;                        // flag de controle do menu
-            int op;                                     // opção que o usuário vai escolher no menu
-            int op1;                                    // opção que o usuário vai escolher de ação com a rede social
-            while(flag){
-                System.out.println("1 - compartilhar um post");
-                System.out.println("2 - postar uma foto");
-                System.out.println("3 - postar um comentário");
-                System.out.println("4 - postar um video");
-                System.out.println("5 - sair do menu");
-                op = in.nextInt();
+                Scanner in = new Scanner(System.in);        // Scanner para entrada de dados
+                boolean flag = true;                        // flag de controle do menu
+                int op;                                     // opção que o usuário vai escolher no menu
+                int op1;                                    // opção que o usuário vai escolher de ação com a rede social
+                int controle1 = 0;                              // var de controle da opção 1
+                int controle2 = 0;                              // var de controle da opção 2
+                int controle3 = 0;                              // var de controle da opção 3
+                int controle4 = 0;                              // var de controle da opção 4
+                while(flag){
+                    System.out.println("1 - compartilhar um post");
+                    System.out.println("2 - postar uma foto");
+                    System.out.println("3 - postar um comentário");
+                    System.out.println("4 - postar um video");
+                    System.out.println("5 - sair do menu");
+                    op = in.nextInt();
 
-                switch (op){
-                    case 1:
-                        System.out.println("Qual das redes sociais voce deseja usar para compartilhar: ");
-                        System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
-                        System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
-                        op1 = in.nextInt();
+                    switch (op){
+                        case 1:     // caso o usuário queira compartilhar
+                            if(controle1 == 0){
+                                System.out.println("Qual das redes sociais voce deseja usar para compartilhar: ");
+                                System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
+                                System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
+                                op1 = in.nextInt();
 
-                        switch (op1){
-                            case 1:
-                                facebook.compartilhar();
-                                break;
-                            case 2:
-                                twitter.compartilhar();
-                                break;
-                        }
+                                switch (op1){
+                                    case 1:
+                                        facebook.compartilhar();
+                                        controle1++;
+                                        break;
+                                    case 2:
+                                        twitter.compartilhar();
+                                        controle1--;
+                                        break;
+                                }
+                            } else {
+                                if (controle1 == 1){
+                                    System.out.println(usuario.getNome() + " você já compartilhou o post no Facebook");
+                                }
+                                if (controle1 == (-1)){
+                                    System.out.println(usuario.getNome() + " você já compartilhou o post no Twitter");
+                                }
+                            }
+                            break;
 
-                        break;
-                    case 2:
-                        System.out.println("Qual das redes sociais voce deseja usar para postar uma foto: ");
-                        System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
-                        System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
-                        op1 = in.nextInt();
+                        case 2:     // caso o usuário queira postar uma foto
+                            if(controle2 == 0){
+                                System.out.println("Qual das redes sociais voce deseja usar para postar uma foto: ");
+                                System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
+                                System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
+                                op1 = in.nextInt();
 
-                        switch (op1){
-                            case 1:
-                                facebook.postarFoto();
-                                break;
-                            case 2:
-                                twitter.postarFoto();
-                                break;
-                        }
+                                switch (op1){
+                                    case 1:
+                                        facebook.postarFoto();
+                                        controle2++;
+                                        break;
+                                    case 2:
+                                        twitter.postarFoto();
+                                        controle2--;
+                                        break;
+                                }
+                            } else {
+                                if (controle2 == 1){
+                                    System.out.println(usuario.getNome() + " você já postou a foto no Facebook");
+                                }
+                                if (controle2 == (-1)){
+                                    System.out.println(usuario.getNome() + " você já postou a foto no Twitter");
+                                }
+                            }
+                            break;
 
-                        break;
-                    case 3:
-                        System.out.println("Qual das redes sociais voce deseja usar para postar um comentario: ");
-                        System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
-                        System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
-                        op1 = in.nextInt();
+                        case 3:     // caso o usuário queira postar um comentário
+                            if(controle3 == 0){
+                                System.out.println("Qual das redes sociais voce deseja usar para postar um comentario: ");
+                                System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
+                                System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
+                                op1 = in.nextInt();
 
-                        switch (op1){
-                            case 1:
-                                facebook.postarComentario();
-                                break;
-                            case 2:
-                                twitter.postarComentario();
-                                break;
-                        }
+                                switch (op1){
+                                    case 1:
+                                        facebook.postarComentario();
+                                        controle3++;
+                                        break;
+                                    case 2:
+                                        twitter.postarComentario();
+                                        controle3--;
+                                        break;
+                                }
+                            } else {
+                                if (controle3 == 1){
+                                    System.out.println(usuario.getNome() + " você já realizou um comentário no Facebook");
+                                }
+                                if (controle3 == (-1)){
+                                    System.out.println(usuario.getNome() + " você já realizou um comentário no Twitter");
+                                }
+                            }
+                            break;
 
-                        break;
+                        case 4:     // caso o usuário queira postar um video
+                            if(controle4 == 0){
+                                System.out.println("Qual das redes sociais voce deseja usar para postar um video: ");
+                                System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
+                                System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
+                                op1 = in.nextInt();
 
-                    case 4:
-                        System.out.println("Qual das redes sociais voce deseja usar para postar um video: ");
-                        System.out.println("1 - " + usuario.redeSociaisUsuario.get(0));
-                        System.out.println("2 - " + usuario.redeSociaisUsuario.get(1));
-                        op1 = in.nextInt();
+                                switch (op1){
+                                    case 1:
+                                        facebook.postarVideo();
+                                        controle4++;
+                                        break;
+                                    case 2:
+                                        twitter.postarVideo();
+                                        controle4--;
+                                        break;
+                                }
+                            } else {
+                                if (controle4 == 1){
+                                    System.out.println(usuario.getNome() + " você já postou o vídeo no Facebook");
+                                }
+                                if (controle4 == (-1)){
+                                    System.out.println(usuario.getNome() + " você já postou o vídeo no Twitter");
+                                }
+                            }
+                            break;
 
-                        switch (op1){
-                            case 1:
-                                facebook.postarVideo();
-                                break;
-                            case 2:
-                                twitter.postarVideo();
-                                break;
-                        }
+                        case 5:
+                            System.out.println("Obrigado por utilizar o sistema!!!");
+                            flag = false;
+                            break;
 
-                        break;
+                        default:
+                            System.out.println("Opção inválida no menu...");
+                    }
 
-                    case 5:
-                        System.out.println("Obrigado por utilizar o sistema!!!");
-                        flag = false;
-                        break;
                 }
-
+            } catch (Exception e) {
+                System.out.println("O erro foi: " + e);
+                System.out.println("Tente novamente...");
+                flag2 = true;
             }
-        } catch (Exception e) {
-            System.out.println("O erro foi: " + e);
         }
 
     }
